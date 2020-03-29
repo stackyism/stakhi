fedd6d0188a3037e0af0f9aacb689c4ae6c7d1e0
Saagar Takhi <stakhi08@gmail.com>
2020-03-29 14:36:42 -0400

    adds styletrong with ssr & baseui

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,20 @@
-import { useRouter } from 'next/router';
-import useSWR from 'swr';
+// DOCUMENTATION: http://styletron.org
 
-function fetcher(url) {
-  return fetch(url).then(r => r.json());
-}
+import { useStyletron } from 'styletron-react'
+import { LightTheme, BaseProvider, styled } from 'baseui';
+import { StatefulInput } from 'baseui/input';
 
-export default function Index() {
-  const { query } = useRouter();
-  const { data, error } = useSWR(
-    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
-    fetcher
-  );
-  // The following line has optional chaining, added in Next.js v9.1.5,
-  // is the same as `data && data.author`
-  const author = data?.author;
-  let quote = data?.quote;
+const Centered = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+});
 
-  if (!data) quote = 'Loading...';
-  if (error) quote = 'Failed to fetch the quote.';
-
-  return (
-    <main className="center">
-      <div className="quote">{quote}</div>
-      {author && <span className="author">- {author}</span>}
-
-      <style jsx>{`
-        main {
-          width: 90%;
-          max-width: 900px;
-          margin: 300px auto;
-          text-align: center;
-        }
-        .quote {
-          font-family: cursive;
-          color: #e243de;
-          font-size: 24px;
-          padding-bottom: 10px;
-        }
-        .author {
-          font-family: sans-serif;
-          color: #559834;
-          font-size: 20px;
-        }
-      `}</style>
-    </main>
-  );
-}
+export default () => (
+  <BaseProvider theme={LightTheme}>
+    <Centered>
+      <StatefulInput />
+    </Centered>
+  </BaseProvider>
+);
